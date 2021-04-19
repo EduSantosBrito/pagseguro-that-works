839c482a53fcc8a8fa1357ef20461d94574068d0
EduSantosBrito <edu.santos.brito@gmail.com>
2021-04-19 16:43:32 -0300

    0.6.0

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,5 @@
 export { default as createPlan } from '~/functions/createPlan';
 export { default as getSession } from '~/functions/getSession';
 export { default as adherePlan } from '~/functions/adherePlan';
+export { default as billingPlan } from '~/functions/billingPlan';
+export { default as retryPayment } from '~/functions/retryPayment';
